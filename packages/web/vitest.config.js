4315d10b2740f7831a5a8db1db37ded094a21f46
schrosis(YumaHonda) <y_honda@yumemi.co.jp>
2025-01-27 10:05:06 +0900

    test: add test

--- packages/web/vitest.config.js
+++ packages/web/vitest.config.js
@@ -1,4 +1,14 @@
 import vitestConfig from "@frontend-coding-exam/common-config/vitest/vitest.config";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react";
 import { defineConfig, mergeConfig } from "vitest/config";
 
-export default mergeConfig(vitestConfig, defineConfig({}));
+export default mergeConfig(
+  vitestConfig,
+  defineConfig({
+    plugins: [react(), vanillaExtractPlugin()],
+    test: {
+      environment: "jsdom",
+    },
+  }),
+);
